eb0c856110f0f774a040a20577e157e7728038d3
Julia.S <der.lavender@gmail.com>
2022-09-12 15:55:17 +0300

    002

---
+++ 001/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void ShowEven(int numberStart, int numberFinish)
+{
+    
+    if(numberStart > numberFinish)
+    {
+        return;
+    }
+    if (numberStart%2 >0) numberStart = numberStart +1;
+    
+    System.Console.WriteLine(numberStart);
+    ShowEven(numberStart+2, numberFinish);
+   
+}
+
+int number1 = Prompt("Введите первое число > ");
+int number2 = Prompt("Введите второе число > ");
+ShowEven(number1 , number2);
---
+++ 002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 15 -> 120
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum (int a, int b)
+{
+    if (a > b)
+    {
+        return 0;
+    }
+    
+    
+    return a+Sum (a+1, b);
+
+}
+
+int a = Prompt("Enter a");
+int b = Prompt("Enter b");
+int result = Sum( a, b);
+System.Console.WriteLine(result);
